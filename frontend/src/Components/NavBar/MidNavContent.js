4b98749fae8ca80d39b80986e0958d9dce03f7f4
noe leiva <noe.leiva@acrometis.com>
2021-01-22 12:18:09 -0500

    added logic to only display logo in large screens

--- frontend/src/Components/NavBar/MidNavContent.js
+++ frontend/src/Components/NavBar/MidNavContent.js
@@ -8,9 +8,7 @@ function MidNavContent() {
     return (
         <>
 
-            {/* <a className="navbar-brand d-none d-lg-block" href="#">Strong Life</a> */}
-            {/* <img src="//static1.squarespace.com/static/5b98025012b13f43168a1e70/t/5f35680e5bf73227161e8b52/1611080942706/?format=1500w"/> */}
-            <img style={{width:125}} src={logo}/>
+            <img style={{width:125}} className="d-none d-lg-block" src={logo}/>
            
         </>
     )
--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -28,7 +28,7 @@ function NavBar() {
 
                         </ul>
 
-                            <MidNavContent/>
+                            <MidNavContent />
     
 
                         <ul className="navbar-nav">
