a2713cd5faece1a61dc6bd3cceb942e9e26e7874
noe leiva <noe.leiva@acrometis.com>
2021-01-22 11:17:05 -0500

    Added the logo to the nav bar

--- frontend/src/Components/Home/IndexHeader.js
+++ frontend/src/Components/Home/IndexHeader.js
@@ -4,16 +4,21 @@ import React from "react";
 function Header () {
 
     return (
-        <div>
-            
-            <div className="jumbotron">
-                <h1 className="display-4">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-4" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p className="lead">
-                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                </p>
+        <div className="row">
+
+            <div className="col-sm-12 col-lg-5 offset-md-1 h-50">
+                <img className="card-img-top snagHomeGymCard" src="..." alt=""/>
+                <div className="card-body snagHomeGymCard">
+                    <h5 className="card-body">Ready to Shatter Your Fitness Plateau?</h5>
+                    <p className="card-text">Join the Beastmode Movement to Strengthen Your Mind and Transform Your Body without leaving the house.</p>
+                    <a href="#" className="btn btn-primary snagHomeGym">SNAG MY HOME GYM MADE EASY GUIDE</a>
+                </div>
+            </div>
+
+            <div className="col-sm-12 col-lg-6">
+
+                <img className="card-img-top" src="https://images.squarespace-cdn.com/content/v1/5b98025012b13f43168a1e70/1601926571415-U2MUBQE9RUHZ1PAHM5QT/ke17ZwdGBToddI8pDm48kIgrOVwg3wcQ-cs-mrkYtXd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcAI91BvIoWEW7Y2dy7jbVmwGjUT3GttF0fpB4_N3aqJlMzCDHTYu1qLyeJlq9ZNWg/beast-mode-banner.jpg?format=1000w" alt=""/>
+   
             </div>
 
         </div>
--- frontend/src/Components/NavBar/MidNavContent.js
+++ frontend/src/Components/NavBar/MidNavContent.js
@@ -1,13 +1,16 @@
 
 
 import React from "react";
+import logo from "../../images/StrongLifeLogo.png"
 
 function MidNavContent() {
 
     return (
         <>
 
-            <a class="navbar-brand d-none d-lg-block" href="#">Navbar</a>
+            {/* <a className="navbar-brand d-none d-lg-block" href="#">Strong Life</a> */}
+            {/* <img src="//static1.squarespace.com/static/5b98025012b13f43168a1e70/t/5f35680e5bf73227161e8b52/1611080942706/?format=1500w"/> */}
+            <img style={{width:125}} src={logo}/>
            
         </>
     )
