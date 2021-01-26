147e228b410753b50003d95ef253b224cac7f857
8connor <connorh16@gmail.com>
2021-01-26 14:58:08 -0500

    Making style changes and removing some unused classes.

--- frontend/src/Components/Home/IndexHeader.js
+++ frontend/src/Components/Home/IndexHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="row">
-      <div className="col-sm-12 col-lg-5 offset-md-1 h-50">
+      <div className="col-sm-12 col-md-5 col-lg-5 offset-md-1 headerCon">
         <img className="card-img-top snagHomeGymCard" src="..." alt="" />
         <div className="card-body snagHomeGymCard">
           <h5 className="card-body">Ready to Shatter Your Fitness Plateau?</h5>
@@ -21,7 +21,6 @@ function Header() {
         <img
           className="card-img-top mt-5"
           src="https://images.squarespace-cdn.com/content/v1/5b98025012b13f43168a1e70/1601926571415-U2MUBQE9RUHZ1PAHM5QT/ke17ZwdGBToddI8pDm48kIgrOVwg3wcQ-cs-mrkYtXd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcAI91BvIoWEW7Y2dy7jbVmwGjUT3GttF0fpB4_N3aqJlMzCDHTYu1qLyeJlq9ZNWg/beast-mode-banner.jpg?format=1000w"
-          alt=""
         />
       </div>
     </div>
