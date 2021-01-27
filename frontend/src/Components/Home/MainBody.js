6cd3a417849648d084d1306fddb3092b0ac9bfed
8connor <connorh16@gmail.com>
2021-01-27 10:55:58 -0500

    Adding the question section to the landing page.

--- frontend/src/Components/Home/MainBody.js
+++ frontend/src/Components/Home/MainBody.js
@@ -1,13 +1,13 @@
-
-import React from "react"
+import React from "react";
+import Questions from "./Questions";
 
 function MainBody(props) {
-
-    return (
-        <div className="main-sec about-us-section parallax">
-            {props.children}
-        </div>
-    )
+  return (
+    <>
+      <div className="main-sec about-us-section parallax">{props.children}</div>
+      <Questions />
+    </>
+  );
 }
 
-export default MainBody
+export default MainBody;
---
+++ frontend/src/Components/Home/Questions.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Questions() {
+  return (
+    <div className="row">
+      <div className="container questionsCon">
+        <div className="col-sm-12 col-md-12 col-lg-12">
+          <h4>
+            Have your attempts to workout from home during the pandemic caused
+            you to lose your mojo? Do you feel isolated with no real plan of
+            attack?
+          </h4>
+          <p>
+            I’ve got good news — You don’t have to head back to the gym to
+            achieve badass-level fitness.
+          </p>
+          <p>
+            Say hello to #beastmode, a simple way to achieve true body and mind
+            transformation through live, virtual workouts.
+          </p>
+          <h4>What’s Beastmode?</h4>
+          <p>
+            Beastmode is a term I use to describe my all-in, no-nonsense method
+            of training. My workouts utilize short bursts of intense activity to
+            help you break through frustrating plateaus and propel you to the
+            next level of strength.
+          </p>
+          <p>
+            You don’t have to think about a thing — just show up for your
+            virtual sessions with me as your trainer and be prepared for a
+            sweatfest!
+          </p>
+          <h4>
+            All that’s required? A beast-like attitude, a few pieces of
+            equipment, and 40 minutes a day.
+          </h4>
+          <a className="btn btn-large bg-success">
+            Snag a free workout session
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Questions;
