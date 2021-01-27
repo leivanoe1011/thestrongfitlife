e85399fbfb821a91957ceedbc1e3ff30831d44b1
8connor <connorh16@gmail.com>
2021-01-27 10:40:23 -0500

    Layout changes to footer as well as book a call button that is on the navbar again.

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -4,8 +4,19 @@ import React from "react";
 function Footer(){
     
     return (
-        <div className="footer">
-            Footer
+        <div className="footer bg-dark">
+            <div className="row">
+                <div className="container mt-4">
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <h4 className="text-light">
+                            Socials
+                        </h4>
+                        <p className="text-light">
+                            hello world
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
