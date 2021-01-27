6c60e14152cca9dbb6956bf6ba8ae54d56327d19
8connor <connorh16@gmail.com>
2021-01-27 13:46:56 -0500

    Update LeftNavContent.js

--- frontend/src/Components/NavBar/LeftNavContent.js
+++ frontend/src/Components/NavBar/LeftNavContent.js
@@ -1,59 +1,7 @@
 import React, { useContext } from "react";
 
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-import { AuthContext } from "../../Context/AuthContext"
 
-
-function LeftNav(){
-
-    const { isAuthenticated } = useContext(AuthContext);
-    // const { isAuthenticated, role } = useContext(AuthContext);
-
-    
-    const unauthenticatedNavBar = () => {
-    
-        return (
-            <>
-                
-                <li className="nav-item">
-                    <Link className="nav-link" to="/#features">MY STORY</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/#contact">PROGRAMS</Link>
-                </li>
-
-    
-            </>
-        )
-    }
-    
-    
-    const authenticatedNavBar = () => {
-        return (
-            <>
-                {/* {(role === "admin") ? adminUserNavLinks() : notAdminUserNavLink()} */}
-    
-                <li className="nav-item">
-                    <Link className="nav-link" to="/dashboard">LEFT</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/#contact">LEFT2</Link>
-                </li>
-
-        
-            </>
-        )
-    }
-    
-    
-    return(
-        <>    
-            {isAuthenticated ? authenticatedNavBar() : unauthenticatedNavBar()}
-
-        </>
-    )
-=======
 import { AuthContext } from "../../Context/AuthContext";
 
 function LeftNav() {
@@ -99,7 +47,6 @@ function LeftNav() {
   return (
     <>{isAuthenticated ? authenticatedNavBar() : unauthenticatedNavBar()}</>
   );
->>>>>>> 67b32dd34bc1e21d64eed987882fba9069f38248
 }
 
 export default LeftNav;
