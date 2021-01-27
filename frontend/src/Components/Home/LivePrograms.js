ca5102234fcc50555bb55b2f330ce9b8b998b09e
8connor <connorh16@gmail.com>
2021-01-27 12:53:58 -0500

    Added the icon to chrome tab. Added the next section with images working on getting the blur affect.

--- frontend/src/Components/Home/LivePrograms.js
+++ frontend/src/Components/Home/LivePrograms.js
@@ -1,15 +1,27 @@
 import React from "react";
+import weight from "../../images/unleash-home.jpg";
+import iphone from "../../images/iphone.jpg";
+import pushup from "../../images/tsfl-home-page.jpg";
 
 function LivePrograms() {
   return (
     <div className="container">
-      <div className="row">
+      <div className="row mb-5">
         <div className="text-center mt-4 mb-4">
-          <h4>
+          <h2>
             If you’re ready to feel unstoppable and take your fitness routine to
             the next level, click below to experience one of my LIVE, virtual
             programs: Unleash the Beast or 1:1 Personal Training.
-          </h4>
+          </h2>
+        </div>
+        <div className="col-sm-12 col-md-4 col-lg-4 liveImg">
+          <img src={weight} className="img-fluid" alt="weight on person's back"></img>
+        </div>
+        <div className="col-sm-12 col-md-4 col-lg-4 liveImg">
+          <img src={iphone} className="img-fluid" alt="person holding iphone"></img>
+        </div>
+        <div className="col-sm-12 col-md-4 col-lg-4 liveImg">
+          <img src={pushup} className="img-fluid" alt="people exercising"></img>
         </div>
       </div>
     </div>
