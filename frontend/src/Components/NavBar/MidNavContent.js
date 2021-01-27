f45eaf0fa93ffe3e16264510489e70c058c0e210
8connor <connorh16@gmail.com>
2021-01-27 10:11:35 -0500

    Made the logo link to the home page.

--- frontend/src/Components/NavBar/MidNavContent.js
+++ frontend/src/Components/NavBar/MidNavContent.js
@@ -1,19 +1,15 @@
-
-
 import React from "react";
-import logo from "../../images/StrongLifeLogo.png"
+import logo from "../../images/StrongLifeLogo.png";
+import {Link} from "react-router-dom";
 
 function MidNavContent() {
-
-    return (
-        <>
-
-            <img style={{width:125}} className="d-none d-lg-block" src={logo}/>
-           
-        </>
-    )
+  return (
+    <>
+      <Link to="/">
+        <img style={{ width: 125 }} className="d-none d-lg-block" src={logo} />
+      </Link>
+    </>
+  );
 }
 
-
 export default MidNavContent;
-
