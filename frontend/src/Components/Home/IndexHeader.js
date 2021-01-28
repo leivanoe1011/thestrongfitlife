c4699d9c0bd7e8d0168a4c0bfa43d18a08635a8e
8connor <connorh16@gmail.com>
2021-01-28 14:14:25 -0500

    Converted most of the code to react-bootstrap.

--- frontend/src/Components/Home/IndexHeader.js
+++ frontend/src/Components/Home/IndexHeader.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Header() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-12 col-md-5 col-lg-5 offset-md-1 headerCon">
+    <Container>
+      <Row>
+        <Col sm={12} md={5} lg={5} className="offset-md-1 headerCon">
           <img className="card-img-top snagHomeGymCard" src="..." alt="" />
           <div className="card-body snagHomeGymCard">
             <h5 className="card-body text-light">
@@ -18,17 +21,17 @@ function Header() {
               SNAG MY HOME GYM MADE EASY GUIDE
             </a>
           </div>
-        </div>
+        </Col>
 
-        <div className="col-sm-12 col-md-6 col-lg-6">
+        <Col sm={12} md={6} lg={6}>
           <img
             alt="header"
             className="img-fluid headImg mt-5"
             src="https://images.squarespace-cdn.com/content/v1/5b98025012b13f43168a1e70/1601926571415-U2MUBQE9RUHZ1PAHM5QT/ke17ZwdGBToddI8pDm48kIgrOVwg3wcQ-cs-mrkYtXd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcAI91BvIoWEW7Y2dy7jbVmwGjUT3GttF0fpB4_N3aqJlMzCDHTYu1qLyeJlq9ZNWg/beast-mode-banner.jpg?format=1000w"
           />
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
