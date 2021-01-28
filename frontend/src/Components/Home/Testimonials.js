28868601304d99da550350e96827bf8cef82d71a
8connor <connorh16@gmail.com>
2021-01-28 12:27:17 -0500

    Added react bootstrap back and the carousel started working. Going to start converting everything to react bootstrap so the code is consistent.

--- frontend/src/Components/Home/Testimonials.js
+++ frontend/src/Components/Home/Testimonials.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Testimonials() {
   return (
@@ -9,49 +12,29 @@ function Testimonials() {
             <h1>LOVE NOTES</h1>
           </div>
         </div>
-        <div className="row">
-          <div
-            id="carouselExampleControls"
-            class="carousel slide"
-            data-bs-ride="carousel"
-          >
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="..." />
-              </div>
-              <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..." />
-              </div>
-              <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..." />
-              </div>
-            </div>
-            <a
-              class="carousel-control-prev"
-              href="#carouselExampleControls"
-              role="button"
-              data-bs-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Previous</span>
-            </a>
-            <a
-              class="carousel-control-next"
-              href="#carouselExampleControls"
-              role="button"
-              data-bs-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Next</span>
-            </a>
-          </div>
-        </div>
+        <Row>
+          <Col>
+            <Carousel>
+              <Carousel.Item className="carousel-item text-center p-4">
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Item>
+              <Carousel.Item className="carousel-item text-center p-4">
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Item>
+              <Carousel.Item className="carousel-item text-center p-4">
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
       </div>
     </div>
   );
