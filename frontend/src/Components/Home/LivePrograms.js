65ef4bdc97a33474de7e00dade429e48fc992f61
8connor <connorh16@gmail.com>
2021-01-28 14:09:31 -0500

    Made the cards look better for different screen sizes.

--- frontend/src/Components/Home/LivePrograms.js
+++ frontend/src/Components/Home/LivePrograms.js
@@ -2,11 +2,15 @@ import React from "react";
 import weight from "../../images/unleash-home.jpg";
 import iphone from "../../images/iphone.jpg";
 import pushup from "../../images/tsfl-home-page.jpg";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
 
 function LivePrograms() {
   return (
-    <div className="container">
-      <div className="row mb-5">
+    <Container>
+      <Row className="mb-5">
         <div className="text-center mt-4 mb-4">
           <h2>
             If you’re ready to feel unstoppable and take your fitness routine to
@@ -15,71 +19,76 @@ function LivePrograms() {
           </h2>
         </div>
 
-        <div className="col-sm-12 col-md-12 col-lg-4 mt-3">
-          <div className="card bg-dark text-white liveImg">
-            <img
+        <Col sm={12} md={12} lg={4} className="mt-3">
+          <Card className="bg-dark text-white liveImg">
+            <Card.Img
+              variant="top"
               src={weight}
               className="img-fluid"
               alt="weight on person's back"
-            ></img>
-            <div className="card-img text-center">
-              <h4 className="text-light mt-2">Unleash The Beast</h4>
-              <p className="card-body text-light">
-                Fitness Membership with Live Workouts, Support, and
-                Accountability.
-              </p>
-              <div className="mb-2">
-                <a className="btn btn-large bg-light" href="/">
-                  TELL ME MORE!
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+            ></Card.Img>
+            <Card.Body className="text-center">
+              <Card.Title className="text-light">Unleash The Beast</Card.Title>
+              <Card.Text className="text-center">
+                <p className="text-light">
+                  Fitness Membership with Live Workouts, Support, and
+                  Accountability.
+                </p>
+              </Card.Text>
+              <a className="btn btn-large bg-light" href="/">
+                TELL ME MORE!
+              </a>
+            </Card.Body>
+          </Card>
+        </Col>
 
-        <div className="col-sm-12 col-md-12 col-lg-4 mt-3">
-          <div className="card bg-dark text-white liveImg">
-            <img
+        <Col sm={12} md={12} lg={4} className="mt-3">
+          <Card className="bg-dark text-white liveImg text-center">
+            <Card.Img
+              variant="top"
               src={iphone}
               className="img-fluid"
               alt="weight on person's back"
-            ></img>
-            <div className="card-img text-center">
-              <h4 className="text-light mt-2">Personal Training</h4>
-              <p className="card-body text-light">
-                1:1 Personalized Online Fitness Coaching.
-              </p>
-              <div className="mb-2">
-                <a className="btn btn-large bg-light" href="/">
-                  I WANT INFO!
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+            ></Card.Img>
+            <Card.Body>
+              <Card.Title className="text-light mt-2">
+                Personal Training
+              </Card.Title>
+              <Card.Text className="text-center">
+                <p className="text-light">
+                  1:1 Personalized Online Fitness Coaching.
+                </p>
+              </Card.Text>
+              <a className="btn btn-large bg-light mt-2" href="/">
+                I WANT INFO!
+              </a>
+            </Card.Body>
+          </Card>
+        </Col>
 
-        <div className="col-sm-12 col-md-12 col-lg-4 mt-3">
-          <div className="card bg-dark text-white liveImg">
-            <img
+        <Col sm={12} md={12} lg={4} className="mt-3">
+          <Card className="bg-dark text-white liveImg text-center">
+            <Card.Img
+              variant="top"
               src={pushup}
               className="img-fluid"
               alt="weight on person's back"
-            ></img>
-            <div className="card-img text-center">
-              <h4 className="text-light mt-2">Testimonials</h4>
-              <p className="card-body text-light">
-                Real Transformation Stories from The Beastmode Community
-              </p>
-              <div className="mb-2">
-                <a className="btn btn-large bg-light" href="/">
-                  BRING ME THERE!
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            ></Card.Img>
+            <Card.Body className="text-center">
+              <Card.Title className="text-light mt-2">Testimonials</Card.Title>
+              <Card.Text>
+                <p className="text-light">
+                  Real Transformation Stories from The Beastmode Community
+                </p>
+              </Card.Text>
+              <a className="btn btn-large bg-light mt-1" href="/">
+                BRING ME THERE!
+              </a>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
